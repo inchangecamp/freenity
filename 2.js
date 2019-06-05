f20f3074644d9796bc2b6e73293601fd2b0da699
inchangecamp <51422353+inchangecamp@users.noreply.github.com>
2019-06-06 00:01:50 +0300

    Delete 2.js

--- 2.js
+++
@@ -1,9 +0,0 @@
-const ffmpeg = require('fluent-ffmpeg')
-
-
-ffmpeg('/home/wizard1988/freenity.news/http/public/files/Big_Buck_Bunny_Trailer.avi')
-  .screenshots({
-    timestamps: ['00:00:02.000'],
-    filename: '123.png',
-    folder: '/home/wizard1988/freenity.news/http'
-  });
