536769e4b32cdef4994fc1c43e57f2f9e7ad1c32
inchangecamp <51422353+inchangecamp@users.noreply.github.com>
2019-06-06 00:01:32 +0300

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs')
-var process = require('child_process')
-var p = process.exec('node /home/wizard1988/freenity.news/http/2.js', function (error, stdout, stderr) {
-  if (error) console.log(error.code)
-})
